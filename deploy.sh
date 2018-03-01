3e470971651b1126ca919ca0bb8f781cbc50d323
Jacek Galowicz <jacek@galowicz.de>
2018-03-01 14:56:49 +0100

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@ stack exec site build
 git fetch --all
 git checkout -b master --track origin/master
 
+find . -type d -depth 1 -not -name ".*" -and -not -name "_site" -exec rm -r {} \;
 mv _site/* .
 
 # Commit
